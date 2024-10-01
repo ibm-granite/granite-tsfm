53d307578b0e2a424f5a3e282150646665b856a2
Stuart Siegel <12914116+ssiegel95@users.noreply.github.com>
2024-10-01 14:43:30 -0400

    remove all s3 interaction code

--- services/finetuning/tsfmfinetuning/ioutils.py
+++ services/finetuning/tsfmfinetuning/ioutils.py
@@ -7,10 +7,7 @@ import io
 import logging
 import os
 import re
-import tempfile
 from io import BytesIO
-from pathlib import Path
-from typing import Optional, Union
 
 import numpy as np
 import pandas as pd
@@ -19,17 +16,6 @@ from transformers import AutoConfig
 # Third Party
 # First Party
 from tsfm_public import TinyTimeMixerConfig
-
-from .ftpayloads import S3aCredentials
-
-
-try:
-    from minio import Minio
-    from minio.deleteobjects import DeleteObject
-
-    HAVE_MINIO = True
-except ImportError as _:
-    HAVE_MINIO = False
 
 
 AutoConfig.register("tinytimemixer", TinyTimeMixerConfig)
@@ -59,106 +45,6 @@ def get_bucket_name(s3_uri):
 def get_object_name(s3_uri):
     bn = get_bucket_name(s3_uri=s3_uri)
     return s3_uri[s3_uri.find(bn) + len(bn) + 1 :]
-
-
-def getminio(s3creds: S3aCredentials) -> Minio:
-    """Obtain a minio client.
-
-    Returns:
-        Minio: A Minio implementation.
-    """
-
-    if not HAVE_MINIO:
-        raise RuntimeError("you must have minio installed to use this method.")
-
-    # Create minio client
-    return Minio(
-        endpoint=s3creds.endpoint,
-        access_key=s3creds.access_key_id,
-        secret_key=s3creds.secret_access_key,
-    )
-
-
-def copy_dir_to_s3(
-    minio_client: Minio,
-    bucket_name: str,
-    local_folder_path: Union[Path, str],
-    prefix: Optional[Union[Path, str]] = None,
-):
-    """Copies a local directory to s3. Note that this will not do any check
-       for pre-existing objects. That is, it will always overwrite existing content
-       of the same name.
-
-    Args:
-        minio_client (Minio): Minio client
-        bucket_name (str): The target bucket name
-        local_folder_path (Path): The local directory. It's best to use a fully qualified path for this.
-        prefix Union[str, Path]: A portion of the local_folder_path that will be retained in the s3 object
-
-    Raises:
-        ValueError: If local_folder_path is not a directory or doesn't exist.
-    """
-    local_folder_path = local_folder_path if isinstance(local_folder_path, Path) else Path(local_folder_path)
-    if not (local_folder_path.is_dir() and local_folder_path.exists()):
-        raise ValueError(f"{local_folder_path} must be a directory and it must exist.")
-
-    # I just LOVE python!
-    if prefix:
-        prefix = prefix if isinstance(prefix, Path) else Path(prefix)
-
-    if not prefix:
-        prefix = os.path.basename(local_folder_path)
-
-    for root, _, files in os.walk(local_folder_path):
-        for file in files:
-            local_file_path = os.path.join(root, file)
-            s3_object_name = os.path.join(prefix, os.path.relpath(local_file_path, local_folder_path))
-            minio_client.fput_object(bucket_name, s3_object_name, local_file_path)
-            LOGGER.info(f"Uploaded {local_file_path} to {s3_object_name} in bucket {bucket_name}")
-
-
-def dump_model_from_s3(
-    s3creds: S3aCredentials,
-    bucket_name: str,
-    model_id: str,
-    model_prefix: str = "model",
-    preprocessor_prefix: str = "preprocessor",
-    target_directory: Path | str = Path(tempfile.gettempdir()),
-):
-    """
-    This method will always dump the model from s3 to the
-    desired target directory.
-    """
-    LOGGER.info(f"in dump_model_from_s3 with model_id {model_id} and target_directory {target_directory}")
-
-    # create the directory holding the model
-    target_directory = target_directory if isinstance(target_directory, Path) else Path(target_directory)
-    model_path = target_directory / f"{model_id}/{model_prefix}"
-    model_path.mkdir(parents=True, exist_ok=True)
-    # create the directory holding the preprocessor
-    preprocessor_path = target_directory / f"{model_id}/{preprocessor_prefix}"
-    preprocessor_path.mkdir(parents=True, exist_ok=True)
-
-    mc = getminio(s3creds)
-    found = False
-    # avoid ambigious prefixes for similarly named models
-    theprefix = model_id if model_id.endswith("/") else model_id + "/"
-    for o in mc.list_objects(bucket_name=bucket_name, prefix=theprefix, recursive=True):
-        if o.is_dir:
-            continue
-        found = True
-        with open(target_directory / o.object_name, mode="wb", buffering=0) as of:
-            object = mc.get_object(bucket_name=bucket_name, object_name=o.object_name)
-            of.write(object.read())  # yes, we have to copy data
-    if not found:
-        err = f"unable to find object {bucket_name}/{model_id}"
-        LOGGER.error(err)
-        raise ValueError(err)
-    # place a breadcrumb file to signal others we've been here
-    # DO NOT ALTER LOCATION OF THIS W/O FIRST CHECKING
-    # WHERE ELSE IT'S USED!
-    with open(model_path / BREADCRUMB_FILE, mode="wb", buffering=0) as _:
-        ...
 
 
 def _to_pandas(iscsv: bool, isfeather: bool, buf: BytesIO = None, timestamp_column: str = None):
@@ -225,27 +111,6 @@ def to_pandas(uri: str, **kwargs) -> pd.DataFrame:
     if not received_bytes and uri[0:7].upper().startswith("FILE://"):
         return _to_pandas(iscsv=_iscsv(uri), isfeather=_isfeather(uri), buf=uri[7:], timestamp_column=timestamp_column)
 
-    # we're reading from s3                                012345
-    if not received_bytes and uri[0:6].upper().startswith("S3A://"):
-        if _iscsv(uri) or _isfeather(uri):
-            bucket_name = get_bucket_name(uri)
-            # object_name = os.path.basename(uri)
-            object_name = uri[uri.index(bucket_name) + len(bucket_name) + 1 :]
-            creds = kwargs.get("s3credentials")
-            creds = (
-                S3aCredentials(**creds)
-                if creds
-                else S3aCredentials(
-                    access_key_id=kwargs.get("access_key_id"),
-                    secret_access_key=kwargs.get("secret_access_key"),
-                    endpoint=kwargs.get("endpoint"),
-                )
-            )
-            mc = getminio(creds)
-            object = mc.get_object(bucket_name=bucket_name, object_name=object_name)
-            buf = BytesIO(object.read())
-            return _to_pandas(_iscsv(uri), _isfeather(uri), buf, timestamp_column=timestamp_column)
-
     # attempt to read a binary blob in different formats
     bio = BytesIO(base64.b64decode(uri)) if not received_bytes else BytesIO(uri)
     answer = _readfeather(bio)
@@ -307,43 +172,3 @@ def getredis():
         password=redis_password,
         # decode_responses=True,
     )
-
-
-def _s3_recursive_delete(mc: Minio, bucket: str, object_prefix: str):
-    # clean up
-    delete_object_list = [
-        lambda x: DeleteObject(x.object_name),
-        mc.list_objects(bucket, object_prefix, recursive=True),
-    ]
-    errors = mc.remove_objects(bucket, delete_object_list)
-    # LOGGER.info(
-    #    f"attempting to delete {mc.list_objects(bucket, object_prefix, recursive=True)}"
-    # )
-    for error in errors:
-        LOGGER.warn("error occurred when deleting object", error)
-
-
-def get_job_result_file(job_id: str, root_dir: str = tempfile.gettempdir()):
-    return Path(root_dir) / f"{job_id}.result.json"
-
-
-def copy_file_to_s3(source_path: str, target_bucket: str, minio_client: Minio):
-    """This will flatten object names on S3 to use only the basename of the source_path.
-    A file named /tmp/dir1/dir2/content.txt will be written to `target_bucket` simply as `content.txt`.
-    """
-    object_name = os.path.basename(source_path)
-    minio_client.fput_object(bucket_name=target_bucket, object_name=object_name, file_path=source_path)
-
-
-def copy_object_from_s3(
-    source_bucket: str,
-    source_object: str,
-    minio_client: Minio,
-    target_directory=tempfile.gettempdir(),
-):
-    """Similarly as copy_file_to_s3, this will not honor the presence of slashe (`/`) in an object name.
-    It will only use the basename of the object writing it directly as
-    target_directory/os.path.basename(object_name).
-    """
-    target_file = Path(target_directory) / os.path.basename(source_object)
-    minio_client.fget_object(bucket_name=source_bucket, object_name=source_object, file_path=target_file)
