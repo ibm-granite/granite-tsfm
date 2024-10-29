58f1279ab67300923073d6536e554a17853f54f0
Wesley Gifford <79663411+wgifford@users.noreply.github.com>
2024-10-29 09:50:30 -0400

    move hf implementation

--- services/inference/tsfminference/service_handler.py
+++ services/inference/tsfminference/service_handler.py
@@ -9,11 +9,7 @@ from pathlib import Path
 from typing import Any, Dict, Optional, Union
 
 import pandas as pd
-from transformers import PretrainedConfig, PreTrainedModel
 
-from tsfm_public import TimeSeriesPreprocessor
-
-from .hfutil import load_config, load_model, register_config
 from .inference_payloads import (
     ForecastingMetadataInput,
     ForecastingParameters,
@@ -150,58 +146,6 @@ class ServiceHandler(ABC):
     ):
         """Abstract method to be implemented by model owner"""
         ...
-
-
-class HuggingFaceHandler(ServiceHandler):
-    def __init__(
-        self,
-        model_id: Union[str, Path],
-        tsfm_config: Dict[str, Any],
-    ):
-        super().__init__(model_id=model_id, tsfm_config=tsfm_config)
-
-        if "model_type" in tsfm_config and "model_config_name" in tsfm_config and "module_path" in tsfm_config:
-            register_config(
-                tsfm_config["model_type"],
-                tsfm_config["model_config_name"],
-                tsfm_config["module_path"],
-            )
-            LOGGER.info(f"registered {tsfm_config['model_type']}")
-
-    def load_preprocessor(self, model_path: str) -> Union[Optional[TimeSeriesPreprocessor], Optional[Exception]]:
-        # load preprocessor
-        try:
-            preprocessor = TimeSeriesPreprocessor.from_pretrained(model_path)
-            LOGGER.info("Successfully loaded preprocessor")
-        except OSError:
-            preprocessor = None
-            LOGGER.info("No preprocessor found")
-        except Exception as ex:
-            raise ex
-
-        return preprocessor
-
-    def load_hf_config(self, model_path: str, **extra_config_kwargs: Dict[str, Any]) -> PretrainedConfig:
-        # load config, separate from load model, since we may need to inspect config first
-        conf = load_config(model_path, **extra_config_kwargs)
-
-        return conf
-
-    def load_hf_model(
-        self, model_path: str, config: PretrainedConfig
-    ) -> Union[Optional[PreTrainedModel], Optional[Exception]]:
-        # load model
-        # TO DO: needs cleanup
-        model, e = load_model(
-            model_path,
-            config=config,
-            module_path=self.tsfm_config.get("module_path", None),
-        )
-
-        if e is not None:
-            raise (e)
-        LOGGER.info("Successfully loaded model")
-        return model
 
 
 def get_service_model_class(config: Dict[str, Any]):
