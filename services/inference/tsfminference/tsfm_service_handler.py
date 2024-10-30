8ff08a4b5f239e917b9a6379533c5a35dfe4e720
Wesley Gifford <79663411+wgifford@users.noreply.github.com>
2024-10-30 14:26:58 -0400

    docstrings, add additional exogenous specifier

--- services/inference/tsfminference/tsfm_service_handler.py
+++ services/inference/tsfminference/tsfm_service_handler.py
@@ -15,6 +15,8 @@ LOGGER = logging.getLogger(__file__)
 
 
 class TinyTimeMixerHandler(HuggingFaceHandler):
+    """Service handler for the tiny time mixer model"""
+
     def _get_config_kwargs(
         self,
         parameters: Optional[ForecastingParameters] = None,
@@ -23,5 +25,7 @@ class TinyTimeMixerHandler(HuggingFaceHandler):
         config_kwargs = {
             "num_input_channels": preprocessor.num_input_channels,
             "prediction_filter_length": parameters.prediction_length,
+            "exogenous_channel_indices": preprocessor.exogenous_channel_indices,
+            "prediction_channel_indices": preprocessor.prediction_channel_indices,
         }
         return config_kwargs
