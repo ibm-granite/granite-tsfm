b28c57774afeb9767edf4b1d3ca2637a3619d5e0
vijaye12 <vijaykr.e@gmail.com>
2024-11-06 09:26:01 -0500

    update notebooks, get_model

--- tsfm_public/toolkit/get_model.py
+++ tsfm_public/toolkit/get_model.py
@@ -50,11 +50,36 @@ def get_model(
     freq_prefix_tuning: bool = None,
     **kwargs,
 ):
+    """
+    TTM Model card offers a suite of models with varying context_length and forecast_length combinations.
+    This wrapper automatically selects the right model based on the given input context_length and prediction_length abstracting away the internal
+    complexity.
+
+    Args:
+        model_path (str):
+            HF model card path or local model path (Ex. ibm-granite/granite-timeseries-ttm-r1)
+        model_name (*optional*, str)
+            model name to use. Allowed values: ttm
+        context_length (int):
+            Input Context length. For ibm-granite/granite-timeseries-ttm-r1, we allow 512 and 1024.
+            For ibm-granite/granite-timeseries-ttm-r2 and  ibm/ttm-research-r2, we allow 512, 1024 and 1536
+        prediction_length (int):
+            Forecast length to predict. For ibm-granite/granite-timeseries-ttm-r1, we can forecast upto 96.
+            For ibm-granite/granite-timeseries-ttm-r2 and  ibm/ttm-research-r2, we can forecast upto 720.
+            Model is trained for fixed forecast lengths (96,192,336,720) and this model add required `prediction_filter_length` to the model instance for required pruning.
+            For Ex. if we need to forecast 150 timepoints given last 512 timepoints using model_path = ibm-granite/granite-timeseries-ttm-r2, then get_model will select the
+            model from 512_192_r2 branch and applies prediction_filter_length = 150 to prune the forecasts from 192 to 150. prediction_filter_length also applies loss
+            only to the pruned forecasts during finetuning.
+        freq_prefix_tuning (*optional*, bool):
+            Future use. Currently do no use this parameter.
+        kwargs:
+            Pass all the extra fine-tuning model parameters intended to be passed in the from_pretrained call to update model configuration.
+    """
     LOGGER.info(f"Loading model from: {model_path}")
 
     if model_name.lower() == "ttm":
         model_path_type = check_ttm_model_path(model_path)
-        prediction_filter_length = 0
+        prediction_filter_length = None
         ttm_model_revision = None
         if model_path_type != 0:
             if context_length is None or prediction_length is None:
@@ -81,7 +106,14 @@ def get_model(
 
             LOGGER.info(f"Selected prediction_length = {selected_prediction_length}")
 
-            prediction_filter_length = prediction_length
+            if selected_prediction_length != prediction_length:
+                prediction_filter_length = prediction_length
+                LOGGER.warning(
+                    f"Requested `prediction_length` ({prediction_length}) is not exactly equal to any of the available TTM prediction lengths.\n\
+                    Hence, TTM will forecast using the `prediction_filter_length` argument to provide the requested prediction length.\n\
+                    Supported context lengths (CL) and forecast/prediction lengths (FL) for Model Card: {model_path} are\n\
+                    {SUPPORTED_LENGTHS[model_path_type]}"
+                )
 
             if freq_prefix_tuning is None:
                 # Default model preference (freq / nofreq)
@@ -92,6 +124,9 @@ def get_model(
                 else:
                     freq_prefix = None
             else:
+                raise Exception(
+                    "In current implementation, set freq_prefix_tuning to None for automatic model selection accordingly.."
+                )
                 if freq_prefix_tuning:
                     freq_prefix = "freq"
                 else:
@@ -116,21 +151,14 @@ def get_model(
                 )
 
         # Load model
-        if prediction_filter_length == 0:
-            model = TinyTimeMixerForPrediction.from_pretrained(model_path, revision=ttm_model_revision, **kwargs)
-        else:
-            LOGGER.warning(
-                f"Requested `prediction_length` ({prediction_length}) is not exactly equal to any of the available TTM prediction lengths.\n\
-                Hence, TTM will forecast using the `prediction_filter_length` argument to provide the requested prediction length.\n\
-                Supported context lengths (CL) and forecast/prediction lengths (FL) for Model Card: {model_path} are\n\
-                {SUPPORTED_LENGTHS[model_path_type]}"
-            )
-            model = TinyTimeMixerForPrediction.from_pretrained(
-                model_path,
-                revision=ttm_model_revision,
-                prediction_filter_length=prediction_filter_length,
-                **kwargs,
-            )
+
+        model = TinyTimeMixerForPrediction.from_pretrained(
+            model_path,
+            revision=ttm_model_revision,
+            prediction_filter_length=prediction_filter_length,
+            **kwargs,
+        )
+
         LOGGER.info("Model loaded successfully!")
         LOGGER.info(
             f"[TTM] context_len = {model.config.context_length}, forecast_len = {model.config.prediction_length}"
