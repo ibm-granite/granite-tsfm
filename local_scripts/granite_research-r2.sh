0744b8b835b8fe9d155cf6d1d7bf4123e400ca82
shota <haoxiang.qiu@ibm.com>
2024-11-11 16:52:20 +0900

    add benchmarking scripts and data configuration files for solar, zafnoo, and exchange datasets

---
+++ local_scripts/granite_research-r2.sh
@@ -0,0 +1,10 @@
+cl=$1
+# for cl in 512 1024 1536; do 
+for fl in 96 192 336 720; do
+    python notebooks/hfdemo/tinytimemixer/full_benchmarking/ttm_full_benchmarking_sh.py --context_length $cl --forecast_length $fl --num_epochs 50 --num_workers 16 \
+    --hf_model_path ibm/ttm-research-r2 \
+    --data_root_path $data_root_path \
+    --enable_prefix_tuning 1 \
+    --save_dir results-research_granite-r2/
+done;
+# done;
