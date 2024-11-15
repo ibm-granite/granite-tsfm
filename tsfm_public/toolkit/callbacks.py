c418f8009817f0452675a1c1a47eacb93d8f578c
Wesley Gifford <79663411+wgifford@users.noreply.github.com>
2024-11-15 14:05:45 -0500

    Docstrings

--- tsfm_public/toolkit/callbacks.py
+++ tsfm_public/toolkit/callbacks.py
@@ -11,7 +11,11 @@ from transformers.training_args import TrainingArguments
 
 
 class TrackingCallback(TrainerCallback):
-    """Simple tracking callback that tracks per epoch run times and calculates some statistics after training completes."""
+    """Simple tracking callback that tracks per epoch run times and calculates some statistics after training completes.
+
+    Args:
+        verbose (bool, optional): If true, prints additional information at the completion of each epoch. Defaults to False.
+    """
 
     def __init__(self, verbose: bool = False):
         self.verbose = verbose
