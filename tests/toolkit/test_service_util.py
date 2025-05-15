4e4f1b3c7f6190a46cd92717b95b95b2a1b2c846
Wesley Gifford <79663411+wgifford@users.noreply.github.com>
2025-05-15 14:38:32 -0400

    add tsp

--- tests/toolkit/test_service_util.py
+++ tests/toolkit/test_service_util.py
@@ -13,16 +13,20 @@ from transformers import (
 )
 
 from tsfm_public.toolkit.service_util import save_deployment_package
+from tsfm_public.toolkit.time_series_preprocessor import TimeSeriesPreprocessor
 
 
 def test_save_deployment_package():
     c = PatchTSTConfig(prediction_length=77)
     m = PatchTSTForPrediction(c)
+    tsp = TimeSeriesPreprocessor(random=42)
 
     with tempfile.TemporaryDirectory() as d:
-        save_deployment_package(d, m)
+        save_deployment_package(d, m, ts_processor=tsp)
         m_new = PatchTSTForPrediction.from_pretrained(d)
         assert m_new.config.prediction_length == m.config.prediction_length
+        tsp_new = TimeSeriesPreprocessor.from_pretrained(d)
+        assert tsp_new.random == tsp.random
 
     c = TimeSeriesTransformerConfig(prediction_length=11)
     m = TimeSeriesTransformerForPrediction(c)
