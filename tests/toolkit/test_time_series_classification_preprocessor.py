10eb3ecaeefca2da0851725cd8211d0cccca7c01
Wesley Gifford <79663411+wgifford@users.noreply.github.com>
2025-05-21 21:50:56 -0400

    catgorical test

--- tests/toolkit/test_time_series_classification_preprocessor.py
+++ tests/toolkit/test_time_series_classification_preprocessor.py
@@ -4,6 +4,7 @@
 """Tests the time series classification preprocessor and functions"""
 
 import numpy as np
+import pytest
 
 from tsfm_public.toolkit.time_series_classification_preprocessor import (
     TimeSeriesClassificationPreprocessor,
@@ -49,6 +50,29 @@ def test_scaling(ts_data_nested):
 
     # to do add condition
     np.testing.assert_almost_equal(df_prep.val.apply(np.mean).mean(), 0)
+
+
+def test_static_categorical(ts_data_nested):
+    df = ts_data_nested.copy()
+    df["cat"] = df.label.copy()
+
+    tsp = TimeSeriesClassificationPreprocessor(
+        timestamp_column="time_date",
+        input_columns=["val", "val2"],
+        label_column="label",
+        static_categorical_columns=["cat"],
+        id_columns=[
+            "id",
+        ],
+        scaling=True,
+    )
+
+    with pytest.raises(Exception):
+        tsp.categorical_vocab_size_list
+
+    tsp.train(df)
+
+    assert tsp.categorical_vocab_size_list == [4]
 
 
 def test_helpers():
