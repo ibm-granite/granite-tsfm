9b77254851717e8e7a83d6f4c46670ee62c1727c
ssiegel95 <12914116+ssiegel95@users.noreply.github.com>
2025-11-06 18:54:09 -0500

    uri_handling probably should move lower down

--- tsfmagentic/packages/tsfm_mcp_server/src/tsfm_mcp_server/tools.py
+++ tsfmagentic/packages/tsfm_mcp_server/src/tsfm_mcp_server/tools.py
@@ -1,7 +1,9 @@
 import logging
+import sys
 import tempfile
 
 import pandas as pd
+from tsfminference.ioutils import path_to_uri
 
 from .datautil import load_timeseries
 from .payloads import DataInput, ForecastResult
@@ -16,6 +18,16 @@ LOGGER = logging.getLogger(__name__)
 
 def forecast_tool(data: DataInput) -> ForecastResult:
     """See docstring in app.py for details."""
+
+    # we should be pydantic validated at this point so no need to re-validate
+    data.data_uri = data.data_uri.strip().upper()
+
+    # some llms consistently send an incorrect uri for win32 platforms so fix it here
+    if sys.platform == "win32":
+        data.data_uri = (
+            data.data_uri.replace("FILE://", "FILE:///") if data.data_uri.find("FILE:///") < 0 else data.data_uri
+        )
+
     ts = load_timeseries(data)
 
     LOGGER.info(f"Loaded time series data with {len(ts)} rows and columns: {ts.columns.tolist()}")
@@ -26,7 +38,7 @@ def forecast_tool(data: DataInput) -> ForecastResult:
     dummy_ts = ts.tail(96)
     timestamp_col = data.timestamp_column
     # get the last timestamp in ts
-    last_timestamp = dummy_ts[timestamp_col].iloc[-1]
+    last_timestamp = ts[timestamp_col].iloc[-1]
     # replace the timestamp column in dummy_ts with last_timestamp + 1 hour increments
     new_timestamps = [last_timestamp + pd.Timedelta(hours=i + 1) for i in range(96)]
     dummy_ts[timestamp_col] = new_timestamps
@@ -35,4 +47,7 @@ def forecast_tool(data: DataInput) -> ForecastResult:
     with tempfile.NamedTemporaryFile(prefix="forecast_result", suffix=".csv", delete=False) as tmp_file:
         dummy_ts.to_csv(tmp_file.name, index=False)
         LOGGER.info(f"Written dummy forecast results to {tmp_file.name}")
-        return ForecastResult(forecast_uri=f"file://{tmp_file.name}", context="Dummy forecast result.")
+        return ForecastResult(
+            forecast_uri=path_to_uri(tmp_file.name),
+            context=f"CSV file with dummy forecast results written to temporary {path_to_uri(tmp_file.name)}.",
+        )
