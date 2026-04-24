5f5d15d78371946a6e0f4613ca9a12113031ce37
Wesley Gifford <79663411+wgifford@users.noreply.github.com>
2026-04-24 08:53:33 -0400

    modern safety check, add test
    Signed-off-by: Wesley Gifford <79663411+wgifford@users.noreply.github.com>

--- tests/toolkit/test_lr_finder.py
+++ tests/toolkit/test_lr_finder.py
@@ -3,11 +3,14 @@
 
 """Tests learning rate finder utility"""
 
+import tempfile
+from pathlib import Path
+
 import numpy as np
 from transformers import set_seed
 
 from tsfm_public.toolkit.dataset import ForecastDFDataset
-from tsfm_public.toolkit.lr_finder import optimal_lr_finder
+from tsfm_public.toolkit.lr_finder import LRFinder, optimal_lr_finder
 from tsfm_public.toolkit.util import select_by_index
 
 
@@ -50,3 +53,27 @@ def test_lr_finder(ttm_base_model, etth_data_base):
     learning_rate, finetune_forecast_model = optimal_lr_finder(
         model, train_dataset, device="mps", batch_size=32, num_iter=10
     )
+
+
+def test_lr_finder_save_load(ttm_base_model):
+    set_seed(42)
+    model = ttm_base_model
+
+    lr_finder = LRFinder(
+        model,
+        opt=None,
+        device="cpu",
+        start_lr=0.01,
+        end_lr=0.001,
+        num_iter=2,
+        step_mode="exp",
+        suggestion="valley",
+        enable_prefix_tuning=False,
+    )
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        tmp_dir = Path(tmp_dir)
+        fname = lr_finder.save("test_model", tmp_dir)
+        lr_finder.load(fname)
+
+        assert isinstance(lr_finder.model, type(model))
